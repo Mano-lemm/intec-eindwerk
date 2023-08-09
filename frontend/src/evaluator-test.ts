d4f9b3a8162ff135ebf644d815ec91e29a493017
Mano Lemmens <lemmensmano@gmail.com>
2023-08-09 10:06:27 +0200

    evaluate prefix bang expressions

--- frontend/src/evaluator-test.ts
+++ frontend/src/evaluator-test.ts
@@ -1,4 +1,9 @@
-import { testEval, testIntegerObject } from "./test-helper.ts";
+import { type mk_Object } from "./object.ts";
+import {
+  testBooleanObject,
+  testEval,
+  testIntegerObject,
+} from "./test-helper.ts";
 
 function testEvalIntegerExpression() {
   const tests: { input: string; expected: number }[] = [
@@ -16,4 +21,41 @@ function testEvalIntegerExpression() {
   }
 }
 
+function testEvalBooleanExpression() {
+  const tests: { input: string; expected: boolean }[] = [
+    { input: "true", expected: true },
+    { input: "false", expected: false },
+  ];
+  let result: mk_Object | undefined;
+  for (const test of tests) {
+    result = testEval(test.input);
+    if (result == undefined) {
+      console.error(`Expected object, got undefined instead.`);
+      continue;
+    }
+    testBooleanObject(result, test.expected);
+  }
+}
+
+function testBangOperator() {
+  const tests: { input: string; expected: boolean }[] = [
+    { input: "!true", expected: false },
+    { input: "!false", expected: true },
+    { input: "!5", expected: false },
+    { input: "!!true", expected: true },
+    { input: "!!5", expected: true },
+  ];
+
+  for (const test of tests) {
+    const result = testEval(test.input);
+    if (result == undefined) {
+      console.error(`Expected object, got undefined instead.`);
+      continue;
+    }
+    testBooleanObject(result, test.expected);
+  }
+}
+
 testEvalIntegerExpression();
+testEvalBooleanExpression();
+testBangOperator();
--- frontend/src/evaluator.ts
+++ frontend/src/evaluator.ts
@@ -6,16 +6,19 @@ import {
   Program,
   type Statement,
   StringLiteral,
+  PrefixExpression,
 } from "./ast.ts";
 import {
   Boolean_OBJ,
   Integer_OBJ,
+  Null_OBJ,
   String_OBJ,
   type mk_Object,
 } from "./object.ts";
 
 const TRUE = new Boolean_OBJ(true);
 const FALSE = new Boolean_OBJ(false);
+const NULL = new Null_OBJ();
 
 export function evaluate(node: Node): mk_Object | undefined {
   if (node instanceof ExpressionStatement) {
@@ -31,7 +34,17 @@ export function evaluate(node: Node): mk_Object | undefined {
     return new String_OBJ(node.val);
   } else if (node instanceof Program) {
     return evalStatements(node.statements);
+  } else if (node instanceof PrefixExpression) {
+    if (node.right == undefined) {
+      return undefined;
+    }
+    const right = evaluate(node.right);
+    if (right == undefined) {
+      return undefined;
+    }
+    return evalPrefixExpression(node.operator, right);
   }
+  return undefined;
   return undefined;
 }
 
@@ -41,4 +54,29 @@ function evalStatements(stmts: Statement[]): mk_Object | undefined {
     result = evaluate(statement);
   }
   return result;
+}
+
+function evalPrefixExpression(
+  oper: string,
+  right: mk_Object
+): mk_Object | undefined {
+  switch (oper) {
+    case "!":
+      return evalBangOperatorExpression(right);
+    default:
+      return undefined;
+  }
+}
+
+function evalBangOperatorExpression(right: mk_Object): mk_Object {
+  switch (right) {
+    case TRUE:
+      return FALSE;
+    case FALSE:
+      return TRUE;
+    case NULL:
+      return TRUE;
+    default:
+      return FALSE;
+  }
 }
