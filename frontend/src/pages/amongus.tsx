9187fec7fcaab1db225a0c56f08b30824535d57e
Mano Lemmens <lemmensmano@gmail.com>
2023-08-17 15:25:15 +0200

    basic draft of user page

--- frontend/src/pages/amongus.tsx
+++ frontend/src/pages/amongus.tsx
@@ -1,4 +1,5 @@
 import { monkey_eval } from "monkey_interpreter"
+import Link from "next/link"
 import { useRef, useState } from "react"
 
 export default function test(){
@@ -14,9 +15,12 @@ export default function test(){
 
     return (
         <>
-            <div className="flex flex-col items-center justify-center h-screen border-cyan-300 border-8">
-                <textarea className="bg-slate-100" ref={rawText} defaultValue="" cols={60} rows={20}></textarea>
-                <button onClick={interpret}>amogus</button>
+            <div className="flex flex-col items-center justify-center h-screen">
+                <textarea className="bg-slate-100 rounded p-2" ref={rawText} defaultValue={`Hello,\nThis is an online monkey interpreter\nhelp can be found via the \"help\" button\n`} cols={60} rows={20}></textarea>
+                <div className="flex p-2 gap-28">
+                    <Link className="bg-sky-600/50 p-2 rounded" href={`https://monkeylang.org/`}>Help</Link>
+                    <button className="bg-sky-600/50 p-2 rounded" onClick={interpret}>Run</button>
+                </div>
                 {text == "" ? <></>: <p>{text}</p>}
             </div>
         </>
